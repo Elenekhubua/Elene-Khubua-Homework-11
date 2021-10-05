47a3d2b78845790c861b2134106e7648292aaa32
Elene khubua <elenekhubua@yahoo.com>
2021-10-06 00:35:57 +0400

    add number2

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+// 1. 
 let cars=['audi','bmw','lexus','volkswagen','porsche','audi']
-for(let i=1; i<cars.length; i++){
+for(let i=1; i<cars.length-1; i++){
     console.log(cars[i]);
 }
+
+
+// 2.
+let names=['Elene', 'Nino', 'Eka', 'Ia','Salome', 'Vika']
+for(let i=0; i<names.length; i++){
+    if(names[i].length>3){
+        console.log(i); 
+    }
+}
+
