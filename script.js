2cd518cfc0f0881bcf829cc04daf0681b18809cc
Elene khubua <elenekhubua@yahoo.com>
2021-10-06 00:54:44 +0400

    add number3

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ for(let i=0; i<names.length; i++){
     }
 }
 
+// 3.
+let numbers=[7,8,12,13,16,25,28]
+for(let i=0; i<numbers.length; i++){
+    if(numbers[i]%2==0){
+        console.log(numbers[i]);
+    }
+}
+
+
+for(let i=0; i<numbers.length; i++){
+    if(numbers[i]%2==1){
+        console.log(numbers[i]);
+    }
+}
